993934815a3d8dcc6434078024e7b2e706d0987e
Marko Dunovic <marko_dunovic@hotmail.com>
2019-01-23 20:01:54 +0100

    minor update
    dodate for za sliek koje rade kako treba

--- angular/script.js
+++ angular/script.js
@@ -3,21 +3,29 @@ app.controller('myCtrl', function ($scope) {
     $scope.mojeIme = "Marko";
     $scope.mojePrezime = "Dunovic";
     $scope.mestoRodjenja = "Novisad";
-    $scope.slika = [
-        {
-            img = "https://img-9gag-fun.9cache.com/photo/aM23KQV_460s_v1.jpg",
-            naziv = "slika 1",
+    $scope.slika = [{
+            img: "https://img-9gag-fun.9cache.com/photo/aM23KQV_460s_v1.jpg",
+            naziv: "slika 1"
         },
         {
-            img = "https://img-9gag-fun.9cache.com/photo/aM23KQV_460s_v1.jpg",
-            naziv = "slika 2",
+            img: "https://img-9gag-fun.9cache.com/photo/aM23KQV_460s_v1.jpg",
+            naziv: "slika 2"
         },
         {
-            img = "https://img-9gag-fun.9cache.com/photo/aM23KQV_460s_v1.jpg",
-            naziv = "slika 3",
+            img: "https://img-9gag-fun.9cache.com/photo/aM23KQV_460s_v1.jpg",
+            naziv: "slika 3"
         }
-    
+
     ];
+});
+app.controller('cont2', function ($scope) {
+
+    $scope.myFunc = function () {
+        $scope.ime2 = "pera";
+        $scope.prezime = "peric";
+        $scope.godine = "1980 godiste";
+        $scope.email = "pera@peric.com";
+    };
 });
 
 //var app2 = angular.module('myApp2',[]);
